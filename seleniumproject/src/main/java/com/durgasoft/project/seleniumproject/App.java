7eb6f2a79fb164d825ffd650735ebc8a62069d8e
Bala Boppudi <subbu509@gmail.com>
2019-01-08 17:09:09 +0530

    added jar/dependencies in pom
    added jar/dependencies in pom

---
+++ seleniumproject/src/main/java/com/durgasoft/project/seleniumproject/App.java
@@ -0,0 +1,13 @@
+package com.durgasoft.project.seleniumproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
