083971170c26a0eb82931d6c8309e0789a37caf4
sushmapalakolanu <psushma3964@gmail.com>
2019-01-18 22:44:39 +0530

    dynamic code in tc_002

--- seleniumproject/src/main/java/com/durgasoft/project/seleniumproject/BaseTest.java
+++ seleniumproject/src/main/java/com/durgasoft/project/seleniumproject/BaseTest.java
@@ -7,6 +7,7 @@ import java.util.Properties;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 
@@ -14,9 +15,13 @@ public class BaseTest
 {
 	public static WebDriver driver;
 	public static String path = "./config.properties";
+	public static Properties prop;
+	public static WebElement e;
+	
+	
 	   
 	
-public static String loaddata(String key) throws IOException
+/*public static String loaddata(String key) throws IOException
 	
 	{ 
 		Properties prop = new Properties();
@@ -24,14 +29,18 @@ public static String loaddata(String key) throws IOException
 		FileInputStream fis = new FileInputStream(f);
 		prop.load(fis);
 		return prop.getProperty(key);
-		
-				
-		
-		
-	}
+	}*/
 	public static void openBrowser(String browser) throws IOException
 	{
-		if(browser.equalsIgnoreCase("chrome"))
+		if(prop == null)
+		{
+			prop = new Properties();
+			File f = new File(path);
+			FileInputStream fis = new FileInputStream(f);
+			prop.load(fis);
+		}
+		
+			/*if(browser.equalsIgnoreCase("chrome"))
 		{
 			// System.setProperty("webdriver.chrome.driver", "C:\\Sushma\\softwares\\drivers\\chromedriver.exe");
 			          // (or)
@@ -41,27 +50,64 @@ public static String loaddata(String key) throws IOException
 			// or
 			//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//drivers//chromedriver.exe");
 			//or
-			/* through properties file */
+			 through properties file 
 			
 			System.setProperty("webdriver.chrome.driver", loaddata("chromeexe"));
 			driver = new ChromeDriver();
 			
+		}*/
+		
+		if(prop.getProperty(browser).equalsIgnoreCase("chrome"))
+		{
+			System.setProperty("webdriver.chrome.driver", prop.getProperty("chromeexe"));
+			driver = new ChromeDriver();
+			
 		}
+		
 	}
 	public static void navigate(String url)
+	
 	{
-		driver.get(url);
+		//driver.get(url);
+		driver.get(prop.getProperty(url));
 	}
 
       public void search(String locator, String value)
       {
-    	  driver.findElement(By.id(locator)).sendKeys(value);
+    	 // driver.findElement(By.id(locator)).sendKeys(value);
+    	 // driver.findElement(By.id(prop.getProperty(locator))).sendKeys(value);
+    	  getElement(locator).sendKeys(value);
+    	  
+    	  
      }
       public void clickOnElement(String locator_xpath)
       {
-    		driver.findElement(By.xpath(locator_xpath)).click();
+    		//driver.findElement(By.xpath(locator_xpath)).click();
+    		//driver.findElement(By.xpath(prop.getProperty(locator_xpath))).click();
+    		getElement( locator_xpath).click();
     		
     	}
+      public  WebElement getElement(String element)
+      {
+    	 // WebElement e;
+    	  
+    	  if(element.endsWith("_id"))
+    	  {
+    		  e = driver.findElement(By.id(prop.getProperty(element)));
+    		  
+    		  
+    	  }
+    	  else if(element.endsWith("_xpath"))
+    	  {
+    		 e = driver.findElement(By.xpath(prop.getProperty(element)));
+    		  
+    	  }
+		return e;
+    	  
+    	  
+    	  
+		
+      }
 
 
 
---
+++ seleniumproject/src/test/java/com/durgasoft/project/seleniumproject/TC_002.java
@@ -0,0 +1,18 @@
+package com.durgasoft.project.seleniumproject;
+
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+public class TC_002 extends BaseTest
+{
+  @Test
+  public void f() throws IOException
+  {
+	  
+	  openBrowser("chromebrowser");
+	  navigate("amazonurl");
+	  search("amazonsearch_id","sony");
+	clickOnElement("click_xpath");
+  }
+}
